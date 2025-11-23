9e279f4f53ada82f2d9004754fab17cf2382d81a
Akmal Fuadi <fuadialakmal@gmail.com>
2025-11-23 20:55:45 +0700

    update

--- src/app/(pages)/destinasi/[id]/form/form.jsx
+++ src/app/(pages)/destinasi/[id]/form/form.jsx
@@ -1,74 +1,76 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { action } from "./action";
-import { InputText } from "@/components/input";
+import { InputText, InputTextarea } from "@/components/input";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Form({ data }) {
-  const [form, setForm] = useState(data);
-  const [errors, setErrors] = useState({});
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
+   const [form, setForm] = useState(data);
+   const [errors, setErrors] = useState({});
+   const [loading, setLoading] = useState(false);
+   const router = useRouter();
 
-  useEffect(() => {
-    setForm(data);
-  }, [data]);
+   useEffect(() => {
+      setForm(data);
+   }, [data]);
 
-  return (
-    <form onSubmit={handleSubmit} className="space-y-6">
-      <div className="space-y-2">
-        <input type="hidden" name="id" value={form?.id || ""} />
-        <InputText
-          label="Nama Destinasi"
-          name="name"
-          value={form?.name}
-          setValue={setForm}
-          errors={errors}
-        />
-        <InputText
-          label="Deskripsi"
-          name="description"
-          value={form?.description}
-          setValue={setForm}
-          errors={errors}
-        />
-        <InputText
-          label="Alamat"
-          name="address"
-          value={form?.address}
-          setValue={setForm}
-          errors={errors}
-        />
-        <InputText
-          label="Koordinat"
-          name="coordinate"
-          value={form?.coordinate}
-          setValue={setForm}
-          errors={errors}
-        />
-      </div>
-      <Button disabled={loading}>{loading ? "Menyimpan..." : "Simpan"}</Button>
-    </form>
-  );
+   return (
+      <form onSubmit={handleSubmit} className="space-y-6">
+         <div className="space-y-2">
+            <input type="hidden" name="id" value={form?.id || ""} />
+            <InputText
+               label="Nama Destinasi"
+               name="name"
+               value={form?.name}
+               setValue={setForm}
+               errors={errors}
+            />
+            <InputTextarea
+               label="Deskripsi"
+               name="description"
+               value={form?.description}
+               setValue={setForm}
+               errors={errors}
+            />
+            <InputText
+               label="Alamat"
+               name="address"
+               value={form?.address}
+               setValue={setForm}
+               errors={errors}
+            />
+            <InputText
+               label="Koordinat"
+               name="coordinate"
+               value={form?.coordinate}
+               setValue={setForm}
+               errors={errors}
+            />
+         </div>
+         <Button disabled={loading}>
+            {loading ? "Menyimpan..." : "Simpan"}
+         </Button>
+      </form>
+   );
 
-  async function handleSubmit(e) {
-    setLoading(true);
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+   async function handleSubmit(e) {
+      setLoading(true);
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
 
-    try {
-      const result = await action(formData);
+      try {
+         const result = await action(formData);
 
-      if (result.success) {
-        router.replace("/destinasi");
-      } else {
-        setErrors(result.errors || {});
-        setLoading(false);
+         if (result.success) {
+            router.replace("/destinasi");
+         } else {
+            setErrors(result.errors || {});
+            setLoading(false);
+         }
+      } catch (error) {
+         console.log("Gagal:", error);
+         setLoading(false);
       }
-    } catch (error) {
-      console.log("Gagal:", error);
-      setLoading(false);
-    }
-  }
+   }
 }
--- src/app/(pages)/destinasi/[id]/form/page.jsx
+++ src/app/(pages)/destinasi/[id]/form/page.jsx
@@ -5,22 +5,24 @@ import { notFound } from "next/navigation";
 import Form from "./form";
 
 export default async function Page({ params }) {
-  const { id } = await params;
+   const { id } = await params;
 
-  const data = await getDestinationById(id);
+   const data = await getDestinationById(id);
 
-  // kalau edit dan tidak ada data...
-  if (id != "new" && !data) {
-    notFound();
-  }
+   // kalau edit dan tidak ada data...
+   if (id != "new" && !data) {
+      notFound();
+   }
 
-  return (
-    <div className="space-y-6">
-      <h1 className="text-xl">{id == "new" ? "Tambah" : "Edit"} Destinasi</h1>
+   return (
+      <div className="w-full mx-auto max-w-xl space-y-6">
+         <h1 className="text-xl">
+            {id == "new" ? "Tambah" : "Edit"} Destinasi
+         </h1>
 
-      <div className="space-y-2 w-full mx-auto max-w-xl">
-        <Form data={data} />
+         <div className="space-y-2 w-full mx-auto max-w-xl">
+            <Form data={data} />
+         </div>
       </div>
-    </div>
-  );
+   );
 }
